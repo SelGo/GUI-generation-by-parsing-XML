f67a278816586c36644a1630f540e301b9663f17
Peter Tsampakiouris <tsampakiourispetros@gmail.com>
2016-01-17 16:48:17 +0200

    Generated JFrame with title 'Frame Title' and default close operation DISPOSE ON CLOSE.

--- src/com/selgo/Main.java
+++ src/com/selgo/Main.java
@@ -1,11 +1,15 @@
 package com.selgo;
 
+import java.awt.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -21,9 +25,11 @@ import org.xml.sax.SAXException;
  
 public class Main {
     private static String jFrame;
+    private static String jPanel;
     private static String setTitle;
-    private static String mappingPropery;
     private static String title;
+    private static String setDefaultCloseOperation;
+    private static String add;
 
     public static void main(String[] args) {
 
@@ -44,26 +50,35 @@ public class Main {
             jFrame = "/widget/instances/instance/instance_api/class";
             Node node = (Node) xPath.compile(jFrame).evaluate(xmlDocument, XPathConstants.NODE);
             String query1 = node.getAttributes().getNamedItem("path").getNodeValue(); //javax.swing.JFrame
-            System.out.println(query1);
 
             setTitle = "/widget/instances/instance/instance_api/class/method";
             Node node2 = (Node) xPath.compile(setTitle).evaluate(xmlDocument, XPathConstants.NODE);
             String query2 = node2.getAttributes().getNamedItem("name").getNodeValue(); //setTitle
-            System.out.println(query2);
 
-            mappingPropery = "/widget/instances/instance/mappings/mapping/mappingProperty[@id='inst_1_prop_1']";
-            Node node3 = (Node) xPath.compile(mappingPropery).evaluate(xmlDocument, XPathConstants.NODE);
-            String query3 = node3.getAttributes().getNamedItem("id").getNodeValue(); //inst_1_prop_1
-            System.out.println(query3);
-
-            title = "/uiModel/widgetResourceModel/widgetResource/instance/property@id=''";
+            title = "/uiModel/widgetResourceModel/widgetResource/instance/property";
             Node node4 = (Node) xPath2.compile(title).evaluate(xmlDocument2, XPathConstants.NODE);
-            Element n = (Element) node4;
-            String query4 = n.getAttribute("name");
-            System.out.println(query4);
+            String query4 = node4.getAttributes().getNamedItem("value").getNodeValue(); // Frame Title
+
+            setDefaultCloseOperation = "/widget/instances/instance/instance_api/class/method[@id='inst_1_cl_1_method_3']";
+            Node node5 = (Node) xPath.compile(setDefaultCloseOperation).evaluate(xmlDocument, XPathConstants.NODE);
+            String query5 = node5.getAttributes().getNamedItem("name").getNodeValue(); //setDefaultCloseOperation
+
+            add = "widget/instances/instance/instance_api/class/method[@id='inst_1_cl_2_method_1']";
+            Node node6 = (Node) xPath.compile(add).evaluate(xmlDocument, XPathConstants.NODE);
+            String query6 = node6.getAttributes().getNamedItem("name").getNodeValue(); // add
+            System.out.println(query6);
 
             Class rootFrame = Class.forName(query1);
             Object rootFrameObject = rootFrame.newInstance();
+            Method setTitle = rootFrame.getMethod(query2, String.class);
+            Method setVisible = rootFrame.getMethod("setVisible", Boolean.TYPE);
+            Method setDefaultCloseOperation = rootFrame.getMethod(query5, Integer.TYPE);
+            Method add = rootFrame.getMethod(query6, Component.class);
+
+            setTitle.invoke(rootFrameObject, query4);
+            setDefaultCloseOperation.invoke(rootFrameObject, JFrame.DISPOSE_ON_CLOSE);
+            setVisible.invoke(rootFrameObject, true);
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -75,7 +90,7 @@ public class Main {
             e.printStackTrace();
         } catch (XPathExpressionException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException /*| NoSuchMethodException*/ | SecurityException | IllegalArgumentException /*| InvocationTargetException*/ ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
